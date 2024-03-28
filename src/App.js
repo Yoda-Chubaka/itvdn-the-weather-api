a51e11500b7f268ce73a3e8ecfc3c5c6fa139d1d
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-03-28 18:45:44 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
   const [data, setData] = useState({});
@@ -9,10 +10,12 @@ function App() {
 
   const searchWeather = (event) => {
     if (event.key === "Enter") {
-
+      axios.get(url).then((response) => {
+        setData(response.data)
+      })
+      setTown('');
     }
   }
-
   return (
     <div className="app">
       <div className="inp-field">
@@ -22,9 +25,58 @@ function App() {
           placeholder="Enter location"
           onKeyDown={searchWeather}
         />
-
       </div>
+      <div className="container">
+        <div className="header">
+          <div className="city">
+            <p>{ data.name}</p>
+          </div>         
+        </div>
+        <div className="temp">
+          {data.main ? (
+            <h1>
+              {data.main.temp.toFixed()}
+              °C
+            </h1>
+          ) : null}
+        </div>
+        <div className="desc">
+          {data.weather ? <p>{ data.weather[0].main}</p> : null}
+        </div>
+      </div>
+      {data.name !== undefined
+        && (
+        <div className="footer">
+          <div className="feels">
+            {data.main ? (
+              <p className="bold">
+                {data.main.feels_like.toFixed()}
+                °C
+              </p>
+              ) : null}
+              <p>Відчувається як</p>            
+          </div>
+          <div className="humidity">
+            {data.main ? (
+              <p className="bold">
+                {data.main.humidity}
+                °C
+              </p>
+              ) : null}
+              <p>Вологість</p>            
+          </div>
+          <div className="wind">
+            {data.main ? (
+              <p className="bold">
+                {`${data.wind.speed} `}
+                М/С
+              </p>
+              ) : null}
+              <p>Вітер</p>            
+            </div>
 
+        </div>
+      )}
 
       
     </div>
