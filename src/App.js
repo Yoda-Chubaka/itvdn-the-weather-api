bd15d719a1259c835293e65d093a6c55aa5e71a3
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-03-28 17:43:38 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { useState } from 'react';
 
 function App() {
-
   const [data, setData] = useState({});
   const [town, setTown] = useState('');
+
+  const key = "db15b3b7e2554d459952a78dcdff61a4";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${town}&units=metric&appid=${key}`;
+
+  const searchWeather = (event) => {
+    if (event.key === "Enter") {
+
+    }
+  }
 
   return (
     <div className="app">
@@ -11,6 +19,8 @@ function App() {
         <input type="text"
           value={town}
           onChange={(event) => setTown(event.target.value)}
+          placeholder="Enter location"
+          onKeyDown={searchWeather}
         />
 
       </div>
